751f5c7e4906f2b9a4c0b510d25bdcfcd46e7513
sudipnext <parajulisudip123@outlook.com>
2025-04-03 23:38:39 +0545

    Update project metadata and rename to JHA Consultancy

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'JHA Services',
+  description: 'Created with Love by JHA',
+  generator: 'sudipnext',
 }
 
 export default function RootLayout({
