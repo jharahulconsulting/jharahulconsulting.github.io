df04ab0d70c91f7dd68e021cb317e8d6cec9ea40
sudipnext <parajulisudip123@outlook.com>
2025-04-03 22:12:13 +0545

    Update Vite configuration to use react-swc plugin, add component tagging for development mode, and set up path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import autoprefixer from "autoprefixer";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/jharahulconsulting.github.io/",
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [autoprefixer()],
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  base: "/",
+  
+}));
